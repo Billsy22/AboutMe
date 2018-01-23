751fd14e0b291310aad43ca0095797c090a4db3e
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-01-22 20:03:59 -0700

    started storyboard. saved to quit and restart xcode

---
+++ InterestsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  InterestsViewController.swift
+//  AboutMe
+//
+//  Created by Taylor Bills on 1/22/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+import UIKit
+
+class InterestsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
